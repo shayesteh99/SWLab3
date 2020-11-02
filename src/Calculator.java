31f63e5a93823d5ce53f7ee613eeda64347d1c89
Shayesteh Arasti <shayesteharasti@gmail.com>
2020-11-02 21:20:01 +0330

    add feature of calculator finished

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public static float ComputeExpression(String exp) {
+        String args[] = exp.split(" ");
+        float first = Float.parseFloat(args[0]);
+        float second = Float.parseFloat(args[2]);
+        if (args[1].equals("+")) {
+            return first + second;
+        }
+        return 0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        System.out.println(command);
+        while (true) {
+            System.out.println("Enter your command:\n");
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+            if (command.equals("calc")) {
+                String exp = scanner.nextLine();
+                float result = Calculator.ComputeExpression(exp);
+                System.out.println(result);
+            }
+        }
     }
 }
